bf1ec784b7e7f6cf7ea5aaf3de45726b633f87ad
ARandomUserN <aforusinski@gmai.com>
2022-06-25 15:50:41 +0200

    Added roles management and access

--- Controllers/RoleManagerController.cs
+++ Controllers/RoleManagerController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -15,12 +16,14 @@ namespace EDziennik.Controllers
         {
             _roleManager = roleManager;
         }
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Index()
         {
             var roles = await _roleManager.Roles.ToListAsync();
             return View(roles);
         }
         [HttpPost]
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> AddRole(string roleName)
         {
             if (roleName != null)
