c60aefa99700d954b5ac3b5b6330056507ae306b
ARandomUserN <aforusinski@gmai.com>
2022-06-30 17:57:59 +0200

    Input limit for marks

--- Models/Mark.cs
+++ Models/Mark.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace EDziennik.Models
     public class Mark
     {
         public int Id { get; set; }
+        [Range(1, 6)]
         public int value { get; set; }
         public string description { get; set; }
         public string studentId { get; set; }
